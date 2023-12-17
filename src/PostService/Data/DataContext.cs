4a1fee8aca49aacdca3537b7131b9511680f434b
Kenneth Cheung <connect366@hotmail.com>
2023-12-17 16:24:34 +0100

    first commit

---
+++ src/PostService/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using PostService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace PostService.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Post> Posts { get; set; }
+
+}
